d98692be63d3acdca729e30e5b7a06ae246618da
Andrew Mitchell <andrewpmitchell7@gmail.com>
2018-08-21 09:02:06 -0400

    handling failure use case and sending notification

--- upload-portfolio-labda.py
+++ upload-portfolio-labda.py
@@ -6,20 +6,27 @@ import mimetypes
 def lambda_handler(event, context):
     
     s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:432839647324:deployPortfolioTopic')
     
-    portfolio_bucket = s3.Bucket('portfolio.andrewmitchell.io')
-    build_bucket = s3.Bucket('portfoliobuild.andrewmitchell.io')
-    
-    portfolio_zip = StringIO.StringIO()
-    
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-       
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-      for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, 
-          ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    
-    return "hello from lambda"
+    try:
+      portfolio_bucket = s3.Bucket('portfolio.andrewmitchell.io')
+      build_bucket = s3.Bucket('portfoliobuild.andrewmitchell.io')
+      
+      portfolio_zip = StringIO.StringIO()
+      
+      build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+         
+      
+      with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+          obj = myzip.open(nm)
+          portfolio_bucket.upload_fileobj(obj, nm, 
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+          portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+      
+      print "Job completed"
+      topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed successfully')
+    except:
+      topic.publish(Subject='Portfolio Deployment Failed', Message='The portfolio was not deployed successfully')
+      raise
